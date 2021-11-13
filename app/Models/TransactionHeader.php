5ad3cc081acafef1c5a28aecf39848525da77011
ogadea <orlando.gadea.r@gmail.com>
2021-11-12 21:09:10 -0600

    Version 1 de Paic-Api

---
+++ app/Models/TransactionHeader.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionHeader extends Model
+{
+    use HasFactory;
+        
+    protected $table='rv_transaction_header';
+
+}
